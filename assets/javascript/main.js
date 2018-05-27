c8e98e2fcb90bd360923e4424d598cbc8003b54e
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-05-26 20:03:50 -0400

    initial js setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,44 @@
+var orpheus = {
+	name: "Orpheus",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+var izanagi = {
+	name: "Izanagi",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+var arsene = {
+	name: "Arsene",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+var nyx = {
+	name: "Nyx Avatar",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+var izanami = {
+	name: "Izanami no Okami",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+var yaldabaoth = {
+	name: "Yaldabaoth",
+	hp: 100,
+	attack: 5,
+	counter: 5
+}
+
+function gameInt() {
+
+}
+
+$(document).ready(function() {
+	gameInit();
+});
